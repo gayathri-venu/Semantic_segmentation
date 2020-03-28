f2bc0985b0609e5edbc1f2f7a209bb1a69892a50
Gayathri Venu <gayathrivenu2000@gmail.com>
2020-03-28 21:31:18 +0530

    Update segment.py

--- segment.py
+++ segment.py
@@ -1,5 +1,5 @@
 # USAGE
-# python segment.py --model enet-cityscapes/enet-model.net --classes enet-cityscapes/enet-classes.txt --colors enet-cityscapes/enet-colors.txt --image images/example_01.png
+# python segment.py --model enet-cityscapes/enet-model.net --classes enet-cityscapes/enet-classes.txt --colors enet-cityscapes/enet-colors.txt --image input/images/example_01.png
 
 # import the necessary packages
 import numpy as np
